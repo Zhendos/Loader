1ca3640a5f19f7d7d4a969d7d324f8dd2773d578
Zhendos <youri_da_bomb@hotmail.com>
2015-02-09 17:30:23 +0100

    Create Camera.java

---
+++ Camera.java
@@ -0,0 +1,63 @@
+package org.bot.reflection;
+
+import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import org.bot.loader.ClassLoader;
+
+
+public class Camera {
+	/**
+	 * 
+	 * 
+	 * --> Gets the cameraX position.
+	 * @return
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 */
+	public static int getCameraX() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
+		Field f = ClassLoader.clazz.getDeclaredField("e");
+		f.setAccessible(true);
+		return (int) f.getInt(ClassLoader.applet);
+	}
+	/**
+	 * 
+	 * 
+	 * --> Gets the CameraY position.
+	 * @return
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 */
+	public static int getCameraY() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
+		Field f = ClassLoader.clazz.getDeclaredField("h");
+		f.setAccessible(true);
+		return (int) f.getInt(ClassLoader.applet);
+	}
+	
+	/**
+	 * 
+	 * --> Gets the CameraZ position.
+	 * @return
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 */
+	public static int getCameraZ() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
+		Field f = ClassLoader.clazz.getDeclaredField("g");
+		f.setAccessible(true);
+		return (int) f.getInt(ClassLoader.applet);
+	}
+	
+	 public void setCameraX(int cameraX) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
+		cameraX = this.getCameraX();
+		
+	}
+	
+}
