a3cb3cfb63b5183432809634e802226cac44584c
Zhendos <youri_da_bomb@hotmail.com>
2015-02-10 17:20:31 +0100

    Create Log.java

---
+++ Log.java
@@ -0,0 +1,176 @@
+started
+Log #0
+CameraX position: 0
+CameraY position: 0
+CameraZ position: 0
+MouseX position: 286
+MouseY position: 30
+MouseIdleTime: 0
+
+Log #1
+CameraX position: 0
+CameraY position: 0
+CameraZ position: 0
+MouseX position: 286
+MouseY position: 30
+MouseIdleTime: 0
+
+Log #2
+CameraX position: 0
+CameraY position: 0
+CameraZ position: 0
+MouseX position: 379
+MouseY position: 175
+MouseIdleTime: 0
+
+Log #3
+CameraX position: 0
+CameraY position: 0
+CameraZ position: 0
+MouseX position: 485
+MouseY position: 285
+MouseIdleTime: 0
+
+Log #4
+CameraX position: 0
+CameraY position: 0
+CameraZ position: 0
+MouseX position: 485
+MouseY position: 285
+MouseIdleTime: 0
+
+Log #5
+CameraX position: 0
+CameraY position: 0
+CameraZ position: 0
+MouseX position: 298
+MouseY position: 333
+MouseIdleTime: 0
+
+Log #6
+CameraX position: 0
+CameraY position: 0
+CameraZ position: 0
+MouseX position: 302
+MouseY position: 327
+MouseIdleTime: 3
+
+Log #7
+CameraX position: 0
+CameraY position: 0
+CameraZ position: 0
+MouseX position: 225
+MouseY position: 243
+MouseIdleTime: 76
+
+Log #8
+CameraX position: 6816
+CameraY position: 5714
+CameraZ position: -658
+MouseX position: 225
+MouseY position: 243
+MouseIdleTime: 181
+
+Log #9
+CameraX position: 6816
+CameraY position: 5714
+CameraZ position: -658
+MouseX position: 225
+MouseY position: 243
+MouseIdleTime: 282
+
+Log #10
+CameraX position: 6816
+CameraY position: 5714
+CameraZ position: -658
+MouseX position: 225
+MouseY position: 243
+MouseIdleTime: 382
+
+Log #11
+CameraX position: 6816
+CameraY position: 5714
+CameraZ position: -658
+MouseX position: 225
+MouseY position: 243
+MouseIdleTime: 480
+
+Log #12
+CameraX position: 6002
+CameraY position: 6630
+CameraZ position: -1707
+MouseX position: 225
+MouseY position: 243
+MouseIdleTime: 2
+
+Log #13
+CameraX position: 7751
+CameraY position: 6488
+CameraZ position: -817
+MouseX position: 235
+MouseY position: 150
+MouseIdleTime: 2
+
+Log #14
+CameraX position: 6361
+CameraY position: 6003
+CameraZ position: -1772
+MouseX position: 212
+MouseY position: 273
+MouseIdleTime: 0
+
+Log #15
+CameraX position: 5705
+CameraY position: 6188
+CameraZ position: -1465
+MouseX position: 330
+MouseY position: 168
+MouseIdleTime: 0
+
+Log #16
+CameraX position: 6844
+CameraY position: 7309
+CameraZ position: -926
+MouseX position: 346
+MouseY position: 152
+MouseIdleTime: 14
+
+Log #17
+CameraX position: 7313
+CameraY position: 6496
+CameraZ position: -1732
+MouseX position: 346
+MouseY position: 152
+MouseIdleTime: 65
+
+Log #18
+CameraX position: 7313
+CameraY position: 6496
+CameraZ position: -1732
+MouseX position: 346
+MouseY position: 152
+MouseIdleTime: 162
+
+Log #19
+CameraX position: 7313
+CameraY position: 6496
+CameraZ position: -1732
+MouseX position: 346
+MouseY position: 152
+MouseIdleTime: 260
+
+Log #20
+CameraX position: 7313
+CameraY position: 6496
+CameraZ position: -1732
+MouseX position: 346
+MouseY position: 152
+MouseIdleTime: 358
+
+Log #21
+CameraX position: 7315
+CameraY position: 6496
+CameraZ position: -1732
+MouseX position: 346
+MouseY position: 152
+MouseIdleTime: 456
