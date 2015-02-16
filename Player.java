6f10509739a30a950d11a447a786cdaa1d29deef
Zhendos <youri_da_bomb@hotmail.com>
2015-02-16 16:14:31 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -7,7 +7,34 @@ import org.bot.loader.ClassLoader;
 public class Player {
 	/**
 	 * 
-	 * Gets the model height.
+	 * Fields.
+	 * 
+	 */
+	public static Object playerInstance;
+	public static boolean b;
+	
+
+	
+	/**
+	 * 
+	 * The player instance;
+	 * @return
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws ClassNotFoundException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 */
+	public static Object playerInstance() throws NoSuchFieldException, SecurityException, ClassNotFoundException, IllegalArgumentException, IllegalAccessException {
+		Field f = ClassLoader.classLoader.loadClass("client").getDeclaredField("EI");
+		f.setAccessible(true);
+		return (Object) f.get(ClassLoader.applet);
+	}
+
+	
+	/**
+	 * 
+	 * Gets the model height. ( broken )
 	 * @return
 	 * @throws NoSuchFieldException
 	 * @throws SecurityException
@@ -18,7 +45,7 @@ public class Player {
 	public static int getModelHeight() throws NoSuchFieldException, SecurityException, ClassNotFoundException, IllegalArgumentException, IllegalAccessException{
 		Field f = ClassLoader.classLoader.loadClass("I").getDeclaredField("I");
 		f.setAccessible(true);
-		return (int) f.get(ClassLoader.applet);
+		return (int) f.get(playerInstance());
 	}
 	/**
 	 * 
@@ -31,10 +58,10 @@ public class Player {
 	 * @throws SecurityException
 	 * @throws ClassNotFoundException
 	 */
-	public static String getMyPrivilege() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException, ClassNotFoundException{
-		Field f = ClassLoader.classLoader.loadClass("client").getDeclaredField("Str1");
+	public static int getMyPrivilege() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException, ClassNotFoundException{
+		Field f = ClassLoader.classLoader.loadClass("client").getDeclaredField("m");
 		f.setAccessible(true);
-		return (String) f.get(ClassLoader.applet);
+		return (int) f.getInt(ClassLoader.applet);
 	}
 	
 	/**
@@ -50,7 +77,7 @@ public class Player {
 	public static int getX() throws NoSuchFieldException, SecurityException, ClassNotFoundException, IllegalArgumentException, IllegalAccessException{
 		Field f = ClassLoader.classLoader.loadClass("client").getDeclaredField("XC");
 		f.setAccessible(true);
-		return (int) f.getInt(ClassLoader.applet);
+		return (int) f.get(ClassLoader.applet);
 	}
 	
 	/**
@@ -71,7 +98,7 @@ public class Player {
 	
 	/**
 	 * 
-	 * Gets the combat level from the local player.
+	 * Gets the combat level. ( was de playerInstance)
 	 * @return
 	 * @throws NoSuchFieldException
 	 * @throws SecurityException
@@ -80,9 +107,10 @@ public class Player {
 	 * @throws IllegalAccessException
 	 */
 	public static int getCombatLevel() throws NoSuchFieldException, SecurityException, ClassNotFoundException, IllegalArgumentException, IllegalAccessException{
-		Field f = ClassLoader.classLoader.loadClass("BZ").getDeclaredField("MI");
+		Field f = ClassLoader.classLoader.loadClass("BZ").getDeclaredField("MI"); //loadClass BZ.
 		f.setAccessible(true);
-		return (int) f.getInt(ClassLoader.applet);
+		return (int) f.get(playerInstance());
+		
 	}
 	
 	/**
@@ -98,8 +126,123 @@ public class Player {
 	public static String getUsername() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException, ClassNotFoundException{
 		Field f = ClassLoader.classLoader.loadClass("BZ").getDeclaredField("KI");
 		f.setAccessible(true);
+		return (String) f.get(playerInstance());
+	}
+	
+	/**
+	 * 
+	 * Gets the password.
+	 * @return
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws ClassNotFoundException
+	 */
+	public static String getPassword() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException, ClassNotFoundException{
+		Field f = ClassLoader.classLoader.loadClass("client").getDeclaredField("tD");
+		f.setAccessible(true);
 		return (String) f.get(ClassLoader.applet);
 	}
+	
+	/**
+	 * 
+	 * Checks if the username is logged in.
+	 * @return
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws ClassNotFoundException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 */
+	public static boolean isLoggedIn() throws NoSuchFieldException, SecurityException, ClassNotFoundException, IllegalArgumentException, IllegalAccessException{
+		Field f = ClassLoader.classLoader.loadClass("client").getDeclaredField("dD");
+		f.setAccessible(true);
+		return (boolean) f.getBoolean(ClassLoader.applet);
+	}
+	
+	/**
+	 * 
+	 * Gets if an admin can mute you or not.
+	 * @return
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws ClassNotFoundException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 */
+	public static boolean canMute() throws NoSuchFieldException, SecurityException, ClassNotFoundException, IllegalArgumentException, IllegalAccessException {
+		Field f = ClassLoader.classLoader.loadClass("client").getDeclaredField("fD");
+		f.setAccessible(true);
+		return (boolean) f.getBoolean(ClassLoader.applet);
+	}
+	
+	/**
+	 * 
+	 * Gets the destinationX of our player.
+	 * @return
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws ClassNotFoundException
+	 */
+	public static int getDestinationX() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException, ClassNotFoundException {
+		Field f = ClassLoader.classLoader.loadClass("client").getDeclaredField("UJ");
+		f.setAccessible(true);
+		return (int) f.getInt(ClassLoader.applet);
+	}
+	
+	/**
+	 * 
+	 * Gets the destinationY of our player.
+	 * @return
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws ClassNotFoundException
+	 */
+	public static int getDestinationY() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException, ClassNotFoundException {
+		Field f = ClassLoader.classLoader.loadClass("client").getDeclaredField("VJ");
+		f.setAccessible(true);
+		return (int) f.getInt(ClassLoader.applet);
+	}
+	
+	/**
+	 * 
+	 * gets the player count.
+	 * @return
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws ClassNotFoundException
+	 */
+	public static int getPlayerCount() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException, ClassNotFoundException {
+		Field f = ClassLoader.classLoader.loadClass("client").getDeclaredField("GI");
+		f.setAccessible(true);
+		return (int) f.getInt(ClassLoader.applet);
+	}
+	
+	/**
+	 * 
+	 * Gets the input string from the local user.
+	 * @return
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws ClassNotFoundException
+	 */
+	public static String getInputString() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException, ClassNotFoundException {
+		Field f = ClassLoader.classLoader.loadClass("client").getDeclaredField("JI");
+		f.setAccessible(true);
+		return (String) f.get(ClassLoader.applet);
+	}
+	
+	
+
 	
 
 }
