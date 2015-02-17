e8af0583dfed62f3dc8e8035dbab95c13bdfcf48
Zhendos <youri_da_bomb@hotmail.com>
2015-02-17 19:50:03 +0100

    Update NPC.java

--- NPC.java
+++ NPC.java
@@ -4,8 +4,34 @@ import java.lang.reflect.Field;
 
 import org.bot.loader.ClassLoader;
 
-public class NPC {
+public class NPC extends Entity{
+	/**
+	 * 
+	 * Fields.
+	 * 
+	 */
+	public static Object NPCInstance;
+	// class LI is NPC class
+	// class II is entity class.
+
 	
+	
+	/**
+	 * 
+	 * the NPC instance.
+	 * @return
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws ClassNotFoundException
+	 */
+	public static Object NPCInstance() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException, ClassNotFoundException{
+		Field f = ClassLoader.classLoader.loadClass("client").getDeclaredField("trim");
+		f.setAccessible(true);
+		return (Object) f.get(ClassLoader.applet);
+	}
+
 	/**
 	 * 
 	 * gets the smallX.
@@ -36,8 +62,24 @@ public class NPC {
 	public static int getInteracting() throws NoSuchFieldException, SecurityException, ClassNotFoundException, IllegalArgumentException, IllegalAccessException{
 		Field f = ClassLoader.classLoader.loadClass("II").getField("z");
 		f.setAccessible(true);
-		return (int) f.getInt(ClassLoader.applet);
+		return (int) f.getInt(NPC.NPCInstance());
 		
+	}
+	
+	/**
+	 * 
+	 * Gets the NPC count in the minimap.
+	 * @return
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws NoSuchFieldException
+	 * @throws SecurityException
+	 * @throws ClassNotFoundException
+	 */
+	public static int getCountNPC() throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException, ClassNotFoundException {
+		Field f = ClassLoader.classLoader.loadClass("client").getDeclaredField("valueOf");
+		f.setAccessible(true);
+		return (int) f.getInt(ClassLoader.applet);
 	}
 
 }
